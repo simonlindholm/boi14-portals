a0ac3da014ce10c9f1c8770925ef6bea8a359dde
Simon Lindholm <simon.lindholm10@gmail.com>
2014-04-16 01:49:09 +0200

    subtask 2

--- gen.sh
+++ gen.sh
@@ -1,7 +1,4 @@
-# TODO: Verify that there are unreachable areas in each subtask
-# TODO: Verify that there is a test case with no portal usage in each subtask
-# TODO: Cases with R != C
-# A case with no portals and a long walk?
+#!/bin/bash
 
 alias python='pypy' # (faster, but optional)
 
@@ -18,8 +15,14 @@ python gen-maze.py 10 10 1 40 14        >$OUT/task.1-07.in   # a third one
 python gen-random.py 10 10 20 0 15      >$OUT/task.1-08.in   # random, some open areas
 
 # Subtask 2 (20 points): 1 <= R <= 50, 1 <= C <= 50
-# no portal usage
-# case where nearest wall is on the edge
+cp manual/single-line.txt                $OUT/task.2-01.in   # single line, no portal usage
+cp manual/walking.txt                    $OUT/task.2-02.in   # no portal usage
+python gen-random.py 50 50 30 0         >$OUT/task.2-03.in   # sparse random
+python gen-empty.py 50 50               >$OUT/task.2-04.in   # large empty areas
+python gen-maze.py 50 50 1 0 4          >$OUT/task.2-05.in   # long path
+python gen-maze.py 50 50 1 240 11       >$OUT/task.2-06.in   # labyrinth with some freedom
+python gen-maze.py 50 50 1 240 16       >$OUT/task.2-07.in   # another one, with a crossing
+python gen-maze.py 50 50 1 700 19       >$OUT/task.2-08.in   # labyrinth with even more freedom
 
 # Subtask 3 (20 points): 1 <= R <= 200, 1 <= C <= 200, and every open square has at least one wall adjacent to it
 cp manual/single-line.txt                $OUT/task.3-01.in   # single line, no portal usage
@@ -27,23 +30,27 @@ cp manual/walking                        $OUT/task.3-02.in   # no portal usage
 python gen-maze.py 200 200 0 0 5        >$OUT/task.3-03.in   # long path
 python gen-maze.py 200 200 0 7000 6     >$OUT/task.3-04.in   # labyrinth with some freedom
 python gen-maze.py 200 200 0 42000 612  >$OUT/task.3-05.in   # labyrinth with even more freedom
+python gen-maze.py 50 50 1 240 16       >$OUT/task.2-07.in   # another one, with a crossing
 
 # Subtask 4 (20 points): 1 <= R <= 200, 1 <= C <= 200
-python gen-random.py 200 200 30 1 9     >$OUT/task.4-01.in   # fractalish
-python gen-random.py 200 200 10000 0 11 >$OUT/task.4-02.in   # dense random
-python gen-random.py 200 200 10 0 12    >$OUT/task.4-03.in   # sparse, no portal usage
-python gen-empty.py 200 200             >$OUT/task.4-04.in   # large empty areas
-python gen-maze.py 200 200 1 0 4        >$OUT/task.4-05.in   # long path
-python gen-maze.py 200 200 1 300 7      >$OUT/task.4-06.in   # labyrinth with some freedom
-python gen-maze.py 200 200 1 3000 7     >$OUT/task.4-07.in   # labyrinth with even more freedom
+cp manual/single-line.txt                $OUT/task.4-01.in   # single line, no portal usage
+python gen-random.py 200 200 30 1 9     >$OUT/task.4-02.in   # fractalish
+python gen-random.py 200 200 10000 0 11 >$OUT/task.4-03.in   # dense random
+python gen-random.py 200 200 10 0 12    >$OUT/task.4-04.in   # sparse, no portal usage
+python gen-empty.py 200 200             >$OUT/task.4-06.in   # large empty areas
+python gen-maze.py 200 200 1 0 4        >$OUT/task.4-06.in   # long path
+python gen-maze.py 200 200 1 300 7      >$OUT/task.4-07.in   # labyrinth with some freedom
+python gen-maze.py 200 200 1 3000 7     >$OUT/task.4-08.in   # labyrinth with even more freedom
 
 # Subtask 5 (20 points): 1 <= R <= 1000, 1 <= C <= 1000
-python gen-random.py 1000 1000 100 0 20 >$OUT/task.5-01.in   # sparse
-python gen-random.py 1000 1000 3 0 23   >$OUT/task.5-02.in   # sparse, no portal usage
-python gen-empty.py 1000 1000           >$OUT/task.5-03.in   # large empty areas
-python gen-maze.py 1000 1000 0 0 10     >$OUT/task.5-04.in   # long path (85k steps) (note: takes quite long to generate)
-python gen-maze.py 1000 1000 1 1000 32  >$OUT/task.5-05.in   # labyrinth with some freedom
-python gen-maze.py 1000 1000 1 30000 27 >$OUT/task.5-06.in   # labyrinth with even more freedom
+cp manual/single-line.txt                $OUT/task.5-01.in   # single line, no portal usage
+python gen-random.py 1000 1000 1200 1 20>$OUT/task.5-02.in   # fractalish
+python gen-random.py 1000 1000 100 0 20 >$OUT/task.5-03.in   # sparse
+python gen-random.py 1000 1000 3 0 23   >$OUT/task.5-04.in   # sparse, no portal usage
+python gen-empty.py 1000 1000           >$OUT/task.5-05.in   # large empty areas
+python gen-maze.py 1000 1000 0 0 10     >$OUT/task.5-06.in   # long path (85k steps) (note: takes quite long to generate)
+python gen-maze.py 1000 1000 1 1000 32  >$OUT/task.5-07.in   # labyrinth with some freedom
+python gen-maze.py 1000 1000 1 30000 27 >$OUT/task.5-08.in   # labyrinth with even more freedom
 
 # Solutions
 g++ -O2 solver.cpp -o solver
